210dd55145e186b83c42a420c1292df3b8b8fe71
pfolus <imjanczalover18@gmail.com>
2017-08-18 15:09:10 +0200

    Add comments (K.Nowak)

--- TodoList.java
+++ TodoList.java
@@ -19,29 +19,29 @@ public class TodoList{
         }
     }
 
-    public void archive() {
+    public void archive() { //operujesz na funkcji po której iterujesz, nie zadziała poprawnie.
         for (int i=0; i<todoItems.size(); i++) {
             try {
                 if (todoItems.get(i).getIsDone()) {
                 todoItems.remove(todoItems.get(i));
                 }
-            } catch(ArrayIndexOutOfBoundsException e) {
+            } catch(ArrayIndexOutOfBoundsException e) { // catch niepotrzebny, jeśli stworzysz pomocniczą listę w której przechowasz indexy do usunięcia
             }
         }
     }
 
-    public String toString() {
+    public String toString() { // metodę toString przeniósłbym do klasy TodoItems
         System.out.println("You saved the following to-do items:\n");
         String result = "";
 
         for (Integer i=0; i<todoItems.size(); i++) {
-            result += getOneLine(i);
+            result += getOneLine(i); // tu brakuje \newline pomiędzy poszczególnymi getOneLine(i), taski wyświetlają się w 1 linii
         }
 
         return result;
     }
 
-    private String getOneLine(Integer i) {
+    private String getOneLine(Integer i) { // tą funkcję zamieniłbym właśnie na metodę toString objektu TodoItem
         String line = Integer.toString(i + 1) + ". ";
 
         if ((todoItems.get(i).getIsDone())) {
